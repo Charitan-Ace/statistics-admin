178818acd3c55759224cbfff8b3cb89877bafcf3
LtMtrunG <s3927071@rmit.edu.vn>
2025-01-14 08:17:55 +0700

    Add filter by time for getting all

--- src/main/java/com/charitan/statistics/statistics/internal/StatisticsInternalAPI.java
+++ src/main/java/com/charitan/statistics/statistics/internal/StatisticsInternalAPI.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public interface StatisticsInternalAPI {
     public InternalStatisticsDto getStatisticsForDonor(UUID donorId);
     public InternalStatisticsDto getStatisticsForCharity(UUID charityId);
-    public Map<String, Double> getStatisticsAll(String category, String isoCode, String continent, String status);
+    public Map<String, Double> getStatisticsAll(String category, String isoCode, String continent, String status, String time);
     public InternalStatisticsDto getMyStatistics();
     public List<UUID> getNewUsers(String time);
 }
